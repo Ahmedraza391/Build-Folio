6633acdcc06a4c0021f8907b990763c11505ca50
AHMED RAZA <0326ahmedaptech@gmail.com>
2024-07-31 16:36:06 +0500

    Updated Database File

---
+++ db_build_folio.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 31, 2024 at 01:35 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_build_folio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(200) NOT NULL,
+  `admin_email` varchar(200) NOT NULL,
+  `admin_password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
+(1, 'Admin', 'admin@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_course`
+--
+
+CREATE TABLE `tbl_course` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_course`
+--
+
+INSERT INTO `tbl_course` (`course_id`, `course_name`) VALUES
+(1, 'Full Stack Development'),
+(2, 'Frontend Development'),
+(3, 'Backend Full Course');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_language`
+--
+
+CREATE TABLE `tbl_language` (
+  `lang_id` int(11) NOT NULL,
+  `lang_name` varchar(200) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_language`
+--
+
+INSERT INTO `tbl_language` (`lang_id`, `lang_name`, `course_id`) VALUES
+(2, 'Html,CSS,JavaScript,Php,Mysql', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_lectures`
+--
+
+CREATE TABLE `tbl_lectures` (
+  `id` int(11) NOT NULL,
+  `videoPath` varchar(200) NOT NULL,
+  `video_thumbnail` text NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `lang_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_lectures`
+--
+
+INSERT INTO `tbl_lectures` (`id`, `videoPath`, `video_thumbnail`, `title`, `description`, `course_id`, `lang_id`) VALUES
+(1, './videos/WhatsApp Video 2024-03-28 at 21.08.42_ee0af8eb.mp4', './videos/muha-ajjan-sL2BRR1cuvM-unsplash.jpg', 'Introduction To Backend', 'Learn Backend Full in Simplest Way And Make Dynamic Projects.', 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `user_email` varchar(200) NOT NULL,
+  `user_password` varchar(200) NOT NULL,
+  `user_image` varchar(200) NOT NULL,
+  `user_status` enum('Activate','Deactivate','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `user_name`, `user_email`, `user_password`, `user_image`, `user_status`) VALUES
+(1, 'hassan', 'hassan@gmail.com', 'hassan121', 'hassan.jpg', 'Activate'),
+(2, 'raza', 'raza@gmail.com', 'raza121', 'raza.jpg', 'Deactivate'),
+(3, 'Muhammad Ali', 'ali@gmail.com', '$2y$10$twM7GsG2fgzUAFOM9gRs.es27SZptSq9LyWRREQHB/n8FWyrGzaAm', '', 'Activate'),
+(4, '', '', '$2y$10$nPqpMhKFMpONJZ8cIyu6rOSO529jMrgUslE6mSIy2JUYFb6o6LCEK', '', 'Activate'),
+(5, 'shuja', 'shuja@gmail.com', '$2y$10$VsA0bZdn/v6FK2y7HxVVsOR1nPYEyL8nEfhgfTsfKdozzQKxWMhta', '', 'Activate');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `tbl_course`
+--
+ALTER TABLE `tbl_course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `tbl_language`
+--
+ALTER TABLE `tbl_language`
+  ADD PRIMARY KEY (`lang_id`);
+
+--
+-- Indexes for table `tbl_lectures`
+--
+ALTER TABLE `tbl_lectures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_course`
+--
+ALTER TABLE `tbl_course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_language`
+--
+ALTER TABLE `tbl_language`
+  MODIFY `lang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_lectures`
+--
+ALTER TABLE `tbl_lectures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
